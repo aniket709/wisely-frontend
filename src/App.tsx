f8cb80f056706ea1e9cc7683b65e294c5008c9db
Aniket <aniket.ranjan709@gmail.com>
2025-06-26 00:19:35 +0530

    fixed the url

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
     setLoading(true);
     setError('');
     try {
-      const res = await axios.get<CountryData>(`http://localhost:10000/api/v1/${name}`);
+      const res = await axios.get<CountryData>(`https://wisely-backend.onrender.com/api/v1/${name}`);
       setData(res.data);
     } catch (err) {
       setError('❌ Country not found.');
